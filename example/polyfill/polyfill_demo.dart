8f3d2f86822510c0ea23ec665149fca1cbb50d71
Matthew Butler <butler.matthew@gmail.com>
2013-11-15 11:09:03 -0400

    Added polyfill example.
    Moved color_picker example to its own directory.

---
+++ example/polyfill/polyfill_demo.dart
@@ -0,0 +1,13 @@
+library colorize.demo.polyfill;
+
+// Import our polyfill library.
+import 'package:colorize/polyfill.dart';
+
+void main() {
+  // Other stuff here
+  
+  // Call colorize to run the polyfill on any input elements with the
+  // 'colorize' class
+  colorizeMe();
+  
+}
